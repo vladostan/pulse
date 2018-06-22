dc3dd919d3f3ec33834733b4d26284d5b4e44453
vladostan <vladostan94@gmail.com>
2018-06-23 01:39:35 +0600

    Removing unnecessary code and files

--- main.m
+++ main.m
@@ -1,28 +1,26 @@
 clc; clear; close;
 
 % Videos
-V = VideoReader('data\Videos\p1_normal.mp4');
-% V = VideoReader('data\Videos\p1_physical.mp4');
+V = VideoReader('data\Videos\pid_normal.mp4');
+% V = VideoReader('data\Videos\pid_physical.mp4');
 
 frameRate = V.FrameRate;
 numFr = V.NumberOfFrames;
 
 [forehead, nose] = roi(V);
-foreheadnose = insertShape(read(V,1), 'rectangle', forehead, 'LineWidth', 5, 'Color', 'red');
-foreheadnose = insertShape(foreheadnose, 'rectangle', nose, 'LineWidth', 5, 'Color', 'red');
-imshow(foreheadnose);
+% foreheadnose = insertShape(read(V,1), 'rectangle', forehead, 'LineWidth', 5, 'Color', 'red');
+% foreheadnose = insertShape(foreheadnose, 'rectangle', nose, 'LineWidth', 5, 'Color', 'red');
+% imshow(foreheadnose);
 
 [x, y] = featureTracking(V, forehead, nose);
-% imshow(insertMarker(foreheadnose,[x(:,1) y(:,1)],'*', 'Size', 5, 'Color', 'green'));
+imshow(insertMarker(foreheadnose,[x(:,1) y(:,1)],'*', 'Size', 5, 'Color', 'green'));
 
-%%
-% save('data/xy_data/p18_physical_xy.mat','x','y');
+% save('data/xy_data/pid_physical_xy.mat','x','y');
 
 samplingRate = 250;
 
 x_interp = cubicSplineInterp(V, x, samplingRate);
 y_interp = cubicSplineInterp(V, y, samplingRate);
-% imshow(insertMarker(foreheadnose,[x_interp(:,1) y_interp(:,1)],'*', 'Size', 5, 'Color', 'green'));
 
 x_stable = removeUnstable(x_interp);
 y_stable = removeUnstable(y_interp);
@@ -32,6 +30,7 @@ y_filtered = temporalFiltering(y_stable);
 
 %% Component Analysis Part
 addpath(genpath('ComponentAnalysis'),'ca_data');
+
 % Perform PCA
 tic
 y_pca = PCA(y_filtered,5);
@@ -41,11 +40,6 @@ time_pca = toc;
 tic
 y_fica = fastica(y_filtered, 'lastEig', 10, 'numOfIC', 5);
 time_fica = toc;
-
-% Perform max-kurtosis ICA
-tic
-y_mkica = mkICA(y_filtered,5);
-time_mkica = toc;
 
 % Perform Jade
 tic
@@ -57,86 +51,4 @@ tic
 y_shibbs = shibbs(y_filtered',5);
 time_shibbs = toc;
 
-% save('data/ca_data/p18_physical_ca.mat','y_pca','y_fica','y_mkica','y_jade','y_shibbs','time_pca','time_fica','time_mkica','time_jade','time_shibbs');
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Other stuff
-x_eigVecs1 = applyPCA(x_filtered, 10);
-y_eigVecs1 = applyPCA(y_filtered, 10);
-
-x_eigVecs2 = applyKICA(x_filtered, 10);
-y_eigVecs2 = applyKICA(y_filtered, 10);
-
-x_eigVecs3 = applyJade(x_filtered, 10);
-y_eigVecs3 = applyJade(y_filtered, 10);
-
-[signal_x, signal_number_x] = signalSelection(x_filtered, x_eigVecs3);
-[signal_y, signal_number_y] = signalSelection(y_filtered, y_eigVecs3);
-
-% peakDetection
-% pks_x = findpeaks(signal_x(:,signal_number_x));
-% pks_y = findpeaks(signal_y(:,signal_number_y));
-
-% :::PULSE:::
-% x-component
-T = size(x_filtered,2);
-
-for i = 1:size(signal_x, 2)
-    figure(i); plot(signal_x(:,i));
-end
-close all;
-
-pks = findpeaks(signal_x(:,1));
-[pks, locs] = findpeaks(signal_x(:,1),'MinPeakHeight',max(pks)/40,'MinPeakDistance',frameRate/(150/60)*T/numFr);
-pulse = 60*frameRate*T/numFr./diff(locs)'
-averagePulse = mean(pulse)
-
-% y-component
-T = size(y_filtered,2);
-
-for i = 1:size(signal_y, 2)
-    figure(i); plot(signal_y(:,i));
-end
-close all;
-
-pks = findpeaks(signal_x(:,1));
-[pks, locs] = findpeaks(signal_y(:,1),'MinPeakHeight',max(pks)/40,'MinPeakDistance',frameRate/(150/60)*T/numFr);
-pulse = 60*frameRate*T/numFr./diff(locs)'
-averagePulse = mean(pulse)
-
-%face
-%True = 53 approximately
-%PCA x = 52.5 av, y = 64.4 av
-%ICA x = 55.22 av, y = 54.48 av
-%Jade x = 52.51 av, y = 53.90 av
-
-%face2
-%True = 54-55 approximately
-%PCA x = 78 av, y = 73 av
-%ICA x = 107 av, y = 83 av
-%Jade x = 106 av, y = 99 av
-
-%V66a
-%True = 66
-%PCA x = 67.14 av, y = 64.50 av
-%ICA x = 63.71 av, y = 77.73 av
-%Jade x = 63.71 av, y = 77.68 av
-
-%V66b
-%True = 66
-%PCA x = 73.2 av, y = 78.7 av
-%ICA x = 81 av, y = 90.4 av
-%Jade x = 73.2 av, y = 78.7 av
-
-%V85
-%True = 85
-%PCA x = 67.2 av, y = 90.5 av
-%ICA x = 89.9 av, y = 89.9 av
-%Jade x = 83.8 av, y = 90.1 av
-
-%V118
-%True = 118
-%PCA x = 92 av, y = 78 av
-%ICA x = 79 av, y = 85 av
-%Jade x = 81 av, y = 95 av
+% save('data/ca_data/pid_physical_ca.mat','y_pca','y_fica','y_jade','y_shibbs','time_pca','time_fica','time_jade','time_shibbs');
--- processData.m
+++ processData.m
@@ -1,108 +1,71 @@
 clc; close all; clear;
 
-addpath(genpath('ComponentAnalysis'),'data');
+addpath(genpath('ComponentAnalysis'), 'data');
 
 % XY DATA
 load('data/xy_data/p1_normal_xy.mat');
-% load('data/xy_data/p1_physical_xy.mat');
+% load('data/xy_data/pid_physical_xy.mat');
 
 % CA DATA
-% load('data/ca_data/p1_normal_ca.mat');
-% load('data/ca_data/p1_physical_ca.mat');
+load('data/ca_data/pid_normal_ca.mat');
+% load('data/ca_data/pid_physical_ca.mat');
 
 % Videos
 V = VideoReader('data/Videos/p1_normal.mp4');
-% V = VideoReader('data/Videos/p1_physical.mp4');
+% V = VideoReader('data/Videos/pid_physical.mp4');
 
  y_interp = cubicSplineInterp(V, y, 250);
 
  y_stable = removeUnstable(y_interp);
-%%
-% 
-%  y_filtered = temporalFiltering(y_stable);
+
+ y_filtered = temporalFiltering(y_stable);
 
 % clear V x y y_interp y_stable 
 
 %% Component Analysis Part
 % Perform PCA
 % tic
-% y_pca = PCA(y_filtered,5);
+y_pca = PCA(y_filtered,5);
 % time_pca = toc;
 
 % Perform Fast ICA
 % tic
-% y_fica = fastica(y_filtered, 'lastEig', 10, 'numOfIC', 5);
+y_fica = fastica(y_filtered, 'lastEig', 10, 'numOfIC', 5);
 % time_fica = toc;
-
-% Perform max-kurtosis ICA
-% tic
-% y_mkica = mkICA(y_filtered,5);
-% time_mkica = toc;
 
 % Perform Jade
 % tic
-% [~,y_jade] = jade(y_filtered,5);
+[~,y_jade] = jade(y_filtered,5);
 % time_jade = toc;
 
 % Perform Shibbs
 % tic
-% y_shibbs = shibbs(y_filtered',5);
+y_shibbs = shibbs(y_filtered',5);
 % time_shibbs = toc;
 
-% Perform fast RADICAL (VERY SLOW!!!)
-% [y_radical, ~] = fast_RADICAL(y_filtered);
-
-%save('faceca.mat','y_pca','y_fica','y_mkica','y_jade','y_shibbs','time_pca','time_fica','time_mkica','time_jade','time_shibbs');
+%save('data/ca_data/pidca.mat','y_pca','y_fica','y_jade','y_shibbs','time_pca','time_fica','time_jade','time_shibbs');
 %csvwrite('v1pca.csv', y_pca');
 %csvwrite('v1fica.csv', y_fica');
-%csvwrite('v1mkica.csv', y_mkica');
 %csvwrite('v1jade.csv', y_jade');
 %csvwrite('v1shibbs.csv', y_shibbs');
 
 %% Plot components
 for i = 1:5
-    subplot(2,3,1)
+    subplot(2,2,1)
         plot(y_pca(i,:))
         title(['PCA ' num2str(time_pca) ' sec'])
         
-    subplot(2,3,2)
+    subplot(2,2,2)
         plot(y_fica(i,:))
         title(['Fast ICA ' num2str(time_fica) ' sec'])
 
-    subplot(2,3,3)
-        plot(real(y_mkica(i,:)))
-        title(['Max-Kurtosis ICA ' num2str(time_mkica) ' sec'])
-
-    subplot(2,3,4)
+    subplot(2,2,3)
         plot(y_jade(i,:))
         title(['Jade ' num2str(time_jade) ' sec'])
 
-    subplot(2,3,5)
+    subplot(2,2,4)
         plot(y_shibbs(i,:))
         title(['Shibss ' num2str(time_shibbs) ' sec'])
 
     ginput(1);
-end
-
-%% Calculate average pulse for every component of each algorithm
-fsamp = 250;
-fprintf('PCA\n')
-for i = 1:size(y_pca,1)
-    averagePulse(y_pca(i,:), fsamp)
-end
-fprintf('FAST ICA\n')
-for i = 1:size(y_fica,1)
-    averagePulse(y_fica(i,:), fsamp)
-end
-fprintf('MAX KURTOSIS ICA\n')
-for i = 1:size(y_mkica,1)
-    averagePulse(y_mkica(i,:), fsamp)
-end
-fprintf('JADE\n')
-for i = 1:size(y_jade,1)
-    averagePulse(y_jade(i,:), fsamp)
-end
-fprintf('SHIBBS\n')
-for i = 1:size(y_shibbs,1)
-    averagePulse(y_shibbs(i,:), fsamp)
 end
